fda8af38d19424f73fd5bca4daf92bd08ed3aad9
falovic <mail@falovic.com>
2022-01-15 02:26:18 +0300

    update

--- test-4.js
+++ test-4.js
@@ -2,42 +2,38 @@ var num = 15.123456789;
 
 // toplamda 3 basamaklı sayı kullan
 
-val = num.toPrecision(3);
+console.log(num.toPrecision(3));
 
 // ondalık kısmı 3 basamakta sınırla
 
-val = num.toFixed(3);
+console.log(num.toFixed(3));
 
 // en yakın sayıya yuvarla
 
-val = Math.round(num);
+console.log(Math.round(num));
 
 // aşağı yuvarla
 
-val = Math.floor(num);
+console.log(Math.floor(num));
 
 // yukarı yuvarla
 
-val = Math.ceil(num);
+console.log(Math.ceil(num));
 
 // 1,2,10,56,20 sayılarından en küçüğü ve en büyüğünü bul
 
-val = Math.min(1, 2, 10, 56, 20);
-val = Math.max(1, 2, 10, 56, 20);
+console.log(Math.min(1, 2, 10, 56, 20));
+console.log(Math.max(1, 2, 10, 56, 20));
 
 // sayı aralığını kullanıcının belirleyeceği rastgele bir sayı üretin.
 
-// var max;
-// var min;
+var min = 50;
+var max = 100;
 
-// val = Math.floor(Math.random()*(max-min);
-
-// console.log(Math.floor(min+Math.random()*(max-min)))
-
-console.log(val);
+console.log(Math.floor(min + Math.random() * (max - min)));
 
 /*
-   Bir personelin yaptığı mesai'ye göre aldığı maaşı hesaplayalım.
+   Bir personelin yaptığı mesaiye göre aldığı maaşı hesaplayalım.
    ** Brüt maaş  : 3700 TL
    ** Brüt mesai : 10.3 TL
 */
@@ -45,14 +41,14 @@ console.log(val);
 // Ağustos ayı mesai toplamı 42 saat ise toplam brüt maaş nedir?
 
 var brüt_maas = 3700;
-var brüt_mesai = 10.3;
+var mesai_ücreti = 10.3;
 var mesai_süre = 42;
-var toplam_brüt = brüt_maas + (brüt_mesai * mesai_süre);
+var toplam_brüt = brüt_maas + (mesai_ücreti * mesai_süre);
 
-console.log("toplam brüt:" + toplam_brüt);
+console.log("toplam brüt:" + toplam_brüt.toFixed(2));
 
 // Brüt maaş üzerinden toplam kesinti oranı %25 ise alınacak toplam net maaş nedir?
 
-var toplam_net = toplam_brüt - (toplam_brüt % 25);
+var toplam_net = toplam_brüt - (toplam_brüt * 0.25);
 
-console.log("toplam net:" + toplam_net);
+console.log("toplam net:" + toplam_net.toFixed(2));
