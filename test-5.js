6230ade2dd92865aa081548f3d673458f22d97c2
falovic <mail@falovic.com>
2022-01-17 22:38:11 +0300

    update

---
+++ test-5.js
@@ -0,0 +1,31 @@
+// Demo : Strings
+
+var sentence = "  Template Literals or template strings is the ability Have multi-line strings without any funny business. ";
+
+var url = "https://falovic.com/ Falovic Web Design x y z";
+
+// cümle kaç karakterlidir ?
+
+
+// kaç kelimeden oluşuyor ?
+
+
+// Tüm cümleyi küçük harfe çevirin.
+
+
+// Cümlenin başındaki ve sonundaki boşlukları siliniz.
+
+
+// '-' karakterini silin.
+
+
+// url'nin içinden str kısmını çıkarınız.
+
+
+// url hangi protocol'u kullanmaktadır ? (http,https)
+
+
+// url, '.com' ifadesini içeriyor mu?
+
+
+// url string ifadesini geçerli bir url olarak düzenleyiniz.
