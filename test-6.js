12ef36dd383268bfc9ab352b62d7dd2c8be77969
falovic <mail@falovic.com>
2022-01-21 16:42:26 +0300

    update

--- test-6.js
+++ test-6.js
@@ -3,6 +3,12 @@
 // "Bmw,Mercedes,Opel,Mazda" elemanlarına sahip bir dizi oluşturun.
 
 var car = ["Bmw","Mercedes","Opel","Mazda"]; 
+console.log(car);
+
+/* 
+var car = new Array("Bmw","Mercedes","Opel","Mazda")
+console.log(car);
+*/
 
 // Dizi kaç elemanlıdır ?
 
@@ -10,7 +16,9 @@ console.log(car.length);
 
 // Dizinin ilk ve son elemanı nedir ?
 
-
+console.log(car[0]);
+console.log(car[3]);
+console.log(car[car.length - 1]);
 
 // "Renault" değerini dizinin sonuna ekleyin.
 
@@ -36,10 +44,12 @@ car.reverse();
 
 car.sort();
 
+console.log(car);
+
 // [1,2,5,80] dizisini sıralayın.
 
-val = [1,2,5,80];
-console.log(val.sort());
+var nmb = [1,2,5,80];
+console.log(nmb.sort());
 
 // "Opel" değeri dizinin bir elemanımıdır ?
 
@@ -48,25 +58,26 @@ console.log(car.includes("Opel"));
 // aşağıdaki ifadeyi diziye çeviriniz.
 
 var str = "Chevrolet,Dacia";
-console.log(str.split(","));
-
-
+var car2 = str.split(",")
+console.log(car2);
 
 // Oluşturulan 2 dizinin elemanlarını bir başka dizi ile birleştirin.
 
+var car3 = car.concat(car2);
+console.log(car3);
+
 // Oluşturulan diziden son 2 elemanı siliniz.
 
-// console.log(arr3.pop());
-// console.log(arr3.pop());
-
-// console.log(arr3.splice(6,2));
+var car4 = car3.length - 1;
+console.log(car4);
+console.log(car3.splice(6,2));
 
 /* Aşağıda verilen elemanları bir dizi içerisinde saklayınız.
      
      studentA : Yiğit Bilgi 2010
      StudentB : Sena Turan  1999
-     studentC : Ahmet Turan 1998   
-
+     studentC : Ahmet Turan 1998
 */
 
-console.log(car);
+
+
