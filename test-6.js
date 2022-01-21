a9b6784c1ef4213ef3deed07647f4959a379a79f
falovic <mail@falovic.com>
2022-01-21 14:24:22 +0300

    update

---
+++ test-6.js
@@ -0,0 +1,57 @@
+// Demo : Arrays
+
+// "Bmw,Mercedes,Opel,Mazda" elemanlarına sahip bir dizi oluşturun.
+
+var car = ["Bmw","Mercedes","Opel","Mazda"]; 
+
+// Dizi kaç elemanlıdır ?
+
+console.log(car.length);
+
+// Dizinin ilk ve son elemanı nedir ?
+
+// "Renault" değerini dizinin sonuna ekleyin.
+
+car.push("Renault");
+
+// "Toyota" değerini dizinin başına ekleyin.
+
+car.unshift("Toyota");
+
+// "Seat" değerini siliniz.
+
+
+
+// "Toyota" değerini siliniz.
+
+car.shift();
+
+// Dizi elemanlarını ters çevirin
+
+// Dizi elemanlarını alfabetik olarak sıralayın.
+
+// [1,2,5,80] dizisini sıralayın.
+
+// "Opel" değeri dizinin bir elemanımıdır ?
+
+// var str = "Chevrolet,Dacia";
+// ifadesini diziye çeviriniz.
+
+// Oluşturulan 2 dizinin elemanlarını bir başka dizi ile birleştirin.
+
+// Oluşturulan diziden son 2 elemanı siliniz.
+
+// console.log(arr3.pop());
+// console.log(arr3.pop());
+
+// console.log(arr3.splice(6,2));
+
+/* Aşağıda verilen elemanları bir dizi içerisinde saklayınız.
+     
+     studentA : Yiğit Bilgi 2010
+     StudentB : Sena Turan  1999
+     studentC : Ahmet Turan 1998   
+
+*/
+
+console.log(car);
