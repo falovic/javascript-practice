bf55e1cc044cc92fad43db4cffdca9653e42f944
falovic <mail@falovic.com>
2022-01-29 04:25:06 +0300

    update

--- test-9.js
+++ test-9.js
@@ -1,36 +1,35 @@
-/* 
-   Demo Loops : Sayı Tahmin Oyunu
+// Demo Loops : Sayı Tahmin Oyunu
 
+/*
    1-10 arası rastgele sayı üretilen bir sayıyı aşağı yukarı ifadeleri ile buldurmaya çalışın.
    ** puanlama yapın.
    ** kullanıcı kaç kerede bileceğini belirtebilsin.
 */
 
-var hak,can;
-var tahmin,sayac=0;
-var sayi = Math.floor((Math.random()*10)+1);
-can = Number(prompt('kaç kerede bileceksiniz ?'));
-hak = can;
+var hak = 5;
+var tahmin;
+var sayac = 0;
+var sayi = Math.floor((Math.random() * 10) + 1);
 
 console.log(sayi);
 
-while(hak>0){
+while (hak > 0) {
     hak--;
     sayac++;
-    tahmin = Number(prompt('bir sayı giriniz'));
+    tahmin = Number(prompt('1-10 arasında bir sayı giriniz.'));
 
-    if(sayi == tahmin){
+    if (sayi == tahmin) {
         console.log(`Tebrikler ${sayac} defada bildiniz.`);
-        console.log(`puan : ${100 - (100/can)*(sayac-1)}`);
+        alert(`Tebrikler ${sayac} defada bildiniz.`);
         break;
-    }else if (sayi > tahmin){
-        console.log('yukarı');
-    }else{
-        console.log('aşağı');
-    }
+    } else if (sayi > tahmin) {
+        console.log('Yukarı');
 
-    if(hak==0){
-        console.log('hakkınız bitti. sayı :'+sayi);
+    } else {
+        console.log('Aşağı');
+
+    }
+    if (hak == 0) {
+        console.log('Hakkınız bitti.')
     }
 }
-
